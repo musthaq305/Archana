67e522447d357b379e3b44c12adfba6e1a9c235b
musthaq305 <musthaqmohamed305@gmail.com>
2023-10-05 16:38:48 +0530

    user enter few lines

---
+++ src/com/sample/Employee.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Data");
+		System.out.println("Sample");
+		
+		
+		
+		
+		
+		
+		
+	}
+}
