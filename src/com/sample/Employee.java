80c7e7fe35143eb956e8d3903c7eb2ec1c124d7d
musthaq305 <musthaqmohamed305@gmail.com>
2023-10-05 17:52:43 +0530

    Bala Added his Code

--- src/com/sample/Employee.java
+++ src/com/sample/Employee.java
@@ -2,6 +2,11 @@ package com.sample;
 
 public class Employee {
 
+	private void sum() {
+    
+		System.out.println("Bala Added his Code");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Data");
